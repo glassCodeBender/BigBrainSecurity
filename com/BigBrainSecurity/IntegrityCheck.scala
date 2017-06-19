03e1f453589aa36ac0a2bc1c9a1efdb5cdb77b53
J. Alexander <xandermoorman@gmail.com>
2017-06-19 06:49:35 -0500

    Update IntegrityCheck.scala

--- com/BigBrainSecurity/IntegrityCheck.scala
+++ com/BigBrainSecurity/IntegrityCheck.scala
@@ -5,19 +5,7 @@ package com.BigBrainSecurity
 	* (#)Version: 1.0
 	* (#)Date: 5/8/2017
 	*
-	* Author's Note: This is going to be an extremely long term project because I still have a lot to learn about
-	* the Volatility Framework in Python and I haven't even started studying file forensics yet. The reason I am using
-	* Scala is because I intend to make this program run with parallel processing. Scala is an extremely difficult
-	* language to learn. If I wasn't already so deep into Scala, I probably would have stuck with Python. Nevertheless,
-	* I'm glad I'm learning Scala because Scala has made me a million times better programmer in general. Nevertheless,
-	* it hurts my brain everyday.
-  *
-	* PROGRAM PURPOSE: To test critical files and see if changes have been made.
-	*
-	* IntegrityCheck.scala is a super class of BigBrainSecurity.scala. This program will be the workhorse behind
-	* BigBrainSecurity's IDS system. BigBrainSecurity.scala, along with the forensic program I'm writing in python
-	* that BigBrainSecurity will call, will do most of the "thinking".
-	*
+	* PROGRAM PURPOSE: Test critical files and see if changes have been made.
 	*/
 
 import com.twitter.hashing.KeyHasher
@@ -42,23 +30,18 @@ object HashGenerator{
 		KeyHasher.FNV1_32.hashKey(byteArray) } // this is a test. The algorithm was not chosen yet.
 } // END HashGenerator
 
-object IntegrityCheck extends FileFun with Setup {
+object IntegrityCheck extends FileFun {
 
-	/**********************************************RUN METHOD**************************************************/
-	def run(): Unit = {
+	/** ********************************************RUN METHOD **************************************************/
+	def run ( ): Unit = {
 
-		// NOTE: Do not declare a val before you put data in it like you would in java.
+		/* These values should be set by the configuration files */
+		val userList = getAllDirs ( "/Users" )
+		val systemList = getAllDirs ( "/System" )
+		val appList = getAllDirs ( "/Applications" )
+		val libList = getAllDirs ( "/Library" )
 
-		/* Prepare a list of files before hashes are generated */
-
-		/* These values should be set by the configuration file. */
-		/* These values should be set by the configuration file. */
-		val userList = getAllDirs("/Users")
-		val systemList = getAllDirs("/System")
-		val appList = getAllDirs("/Applications")
-		val libList = getAllDirs("/Library")
-
-		val fullDirArray: Array[String] = ((userList ++ systemList) ++ (appList ++ libList))
+		val fullDirArray: Array[ String ] = ( ( userList ++ systemList ) ++ ( appList ++ libList ) )
 		// If this program is going to run on parallel cores, one of the TreeMaps needs to get split up and
 		// the other other TreeMap should stay intact. This ensures that all of the comparisons work.
 		// During the comparisons we should extract the values that were not presents or that do not match into new Tree.
@@ -74,44 +57,43 @@ object IntegrityCheck extends FileFun with Setup {
 		/* Import main.com.paranoidking.BigBrainSecurity Log File and date for previous log based on data in the config file. */
 
 		/* Export new Map and concatenate the result of Integrity Check. */
+	} // END run()
 
-		// DO NOT CHANGE!!!!
-		def getAllFiles(directories: Array[String]): Array[String] = {
-			def loop(dir: Array[String], accArray: Array[String]): Array[String] = {
-				if (dir.isEmpty) accArray
-				else loop(dir.tail, accArray ++ getFileArray(dir.head))
+		def getAllFiles ( directories: Array[ String ] ): Array[ String ] = {
+			def loop ( dir: Array[ String ], accArray: Array[ String ] ): Array[ String ] = {
+				if ( dir.isEmpty ) accArray
+				else loop ( dir.tail, accArray ++ super.getFileArray ( dir.head ) )
 			}
-			loop(directories, Array[String]())
-		} // END getFullFileList
-		val allFiles = getAllFiles(fullDirArray)
+			loop ( directories, Array [ String ]( ) )
+		} // END getAllFiles()
 
 		/** ****************************************STORE IN Hash Table *******************************************************/
-		/*
-		 * Methods to create Maps from filename Strings to their hash values.
-		 * Currently SHA-256 is used for the checksums, but the algorithm might change.
+		/**
+		 * Methods to create Maps from filename Strings to their SHA-256 hash values.
 		 */
-		def genMap( fileSet: Seq[ String ] ): HashMap[ String, String ] = {
-			def loop( fileSet: Seq[ String ], accMap: HashMap[ String, String ] ): HashMap[ String, String ] = {
+		def genMap ( fileSet: Seq[ String ] ): HashMap[ String, String ] = {
+			def loop ( fileSet: Seq[ String ], accMap: HashMap[ String, String ] ): HashMap[ String, String ] = {
 				// val hashMapAcc = new HashMap(fileSet.head -> makeHash(fileSet.head))
 				if ( fileSet.isEmpty ) accMap
-				else loop( fileSet.tail, accMap + ( fileSet.head -> HashGenerator.generate( fileSet.head ) ) )
+				else loop ( fileSet.tail, accMap + ( fileSet.head -> HashGenerator.generate ( fileSet.head ) ) )
 			} // END loop()
-			loop( fileSet, new HashMap[ String, String ]( ) )
+			loop ( fileSet, new HashMap[ String, String ]( ) )
 		} // END genMap()
 
-		def genTreeMap( fileSet: Seq[ String ] )( implicit ord: Ordering[ String ] ): TreeMap[ String, String ] = {
-			def loop( fileSet: Seq[ String ], accTreeMap: TreeMap[ String, String ] ): TreeMap[ String, String ] = {
+		def genTreeMap ( fileSet: Seq[ String ] )( implicit ord: Ordering[ String ] ): TreeMap[ String, String ] = {
+			def loop ( fileSet: Seq[ String ], accTreeMap: TreeMap[ String, String ] ): TreeMap[ String, String ] = {
 				if ( fileSet.isEmpty ) accTreeMap
-				else loop( fileSet.tail, accTreeMap + ( fileSet.head -> HashGenerator.generate( fileSet.head ) ) )
+				else loop ( fileSet.tail, accTreeMap + ( fileSet.head -> HashGenerator.generate ( fileSet.head ) ) )
 			} // END loop()
-			loop( fileSet, new TreeMap[ String, String ]( ) )
-		} // END genMap()
+			loop ( fileSet, new TreeMap[ String, String ]( ) )
+		} // END genTreeMap()
+  
+		def genTreeMapTwitter ( fileSet: Seq[ String ] )( implicit ord: Ordering[ String ] ): TreeMap[ String, Long ] = {
+			def loop ( fileSet: Seq[ String ], accTreeMap: TreeMap[ String, Long ] ): TreeMap[ String, Long ] = {
+				if ( fileSet.isEmpty ) accTreeMap
+				else loop ( fileSet.tail, accTreeMap + ( fileSet.head -> HashGenerator.makeTwitterHash ( fileSet.head ) ) )
+			} // END loop()
+			loop ( fileSet, new TreeMap[ String, Long ]( ) )
+		} // END genTreeMapTwitter()
 
-		def genTreeMapTwitter( fileSet: Seq[ String ] )( implicit ord: Ordering[ String ] ): TreeMap[ String, Long ] = {
-			def loop( fileSet: Seq[ String ], accTreeMap: TreeMap[ String, Long ] ): TreeMap[ String, Long ] = {
-				if ( fileSet.isEmpty ) accTreeMap
-				else loop( fileSet.tail, accTreeMap + ( fileSet.head -> HashGenerator.makeTwitterHash( fileSet.head ) ) )
-			} // END loop()
-			loop( fileSet, new TreeMap[ String, Long ]( ) )
-		} // END genMap()
-	} // END IntegrityCheck.scala
+}// END IntegrityCheck.scala

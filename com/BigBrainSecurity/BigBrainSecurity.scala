e24482837def36cbb7771460630d3f0bf99eb4cc
J. Alexander <xandermoorman@gmail.com>
2017-07-01 07:45:55 -0500

    Update BigBrainSecurity.scala

--- com/BigBrainSecurity/BigBrainSecurity.scala
+++ com/BigBrainSecurity/BigBrainSecurity.scala
@@ -53,28 +53,18 @@ class BigBrainSecurity extends Setup {
 		val configMap = super.getConfig()
 
 		/* Find file locations from config.txt */
-		val mftTable = configMap("mft_csv_location")
-		val regexTxtFile = configMap("text_file_with_values_to_include_in_output")
-		val prefetchDirectory = configMap("prefetch_csv_directory_location")
-		val outputCSVName = configMap("filtered_csv_output_location")
-		val allCSVDir = configMap("all_csv_output_destination_directory")
-		val safePrefetchList = configMap("safe_prefetch_list")
-
-		/* Take config.txt input and place values in variables.  */
-		val createIntIndex: Boolean =  configMap("create_integer_index").toBoolean
-		val filterSupicious: Boolean = configMap("filter_suspicious").toBoolean
-		val defaultFilter: Boolean = configMap("default_filter").toBoolean
-
-		// This is how this program will be used in the rest of the program.
-		lazy val startIndex = configMap("start_index")
-		lazy val endIndex = configMap("end_index")
-		lazy val startTime = configMap("start_time")
-		lazy val endTime = configMap("end_time")
-
+		val prefetchDirectory = configMap("prefetch_csv_directory_location").get
+		val safePrefetchList = configMap("safe_prefetch_list").get
+		
 		/****************************MAIN METHOD CALLS***************************/
 
 		/* Generate an Array of filenames that the user should check for tampering */
-		private val analyzePrefResult: ParArray[String] = AnalyzePrefetch.analyze(prefetchDirectory, safePrefetchList)
+		private val analyzePrefResult: ParArray[String] = new AnalyzePrefetch(prefetchDirectory, safePrefetchList).analyze
+
+		
+		println("WARNING: Prefetch files from Windows 8 and 10 will give inaccurate results.\n" 
+		+ "Only files from Windows 7 systems and earlier will give accurate results. ")
+		analyzePrefResult.foreach(println)
 
 		/* Clean up MFT csv with CleanMFT.scala*/
 		val cleanedMFT = new CleanMFT(spark)

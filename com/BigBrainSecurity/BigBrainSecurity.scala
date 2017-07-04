5bf43ee1df2a5ccebb702fcdcf199626dfe01224
J. Alexander <xandermoorman@gmail.com>
2017-07-03 22:06:04 -0500

    Update BigBrainSecurity.scala

--- com/BigBrainSecurity/BigBrainSecurity.scala
+++ com/BigBrainSecurity/BigBrainSecurity.scala
@@ -3,7 +3,16 @@ package com.BigBrainSecurity
 /**
   * (@)Author: J. Alexander
   * (#)Version: 1.0
-  * (#)Date: 6/11/2017
+  * (#)Date: 7/3/2017
+	* 
+	* CASE CLASSES: 
+	* Brain
+	* Findings
+	* Registration
+	* User
+	* Technical
+	* 
+	* MAIN CLASS BigBrainSecurity
 	*
   * PROGRAM PURPOSE: This is the driver program for Big Brain Security
 	* forensics and IDS software.
@@ -21,6 +30,85 @@ import scala.io.Source
 import scala.collection.parallel.mutable.ParArray
 import com.BigBrainSecurity.{ AnalyzePrefetch, CleanMFT, IntegrityCheck }
 import org.apache.spark._
+
+/**
+	* case class Brain
+	* Purpose: Contains the raw data from a single time the program run.
+	* This is the data that is sent over from the client.
+	* Eventually the brain will communicate w/ a program that analyzes
+	* raw memory dumps.
+	* @param id primary key
+	* @param dateTime primary key Stores current date.
+	* @param name contains user's name
+	* @param mftCSV contains the mft in csv format
+	* @param regCSV contains registry in csv format.
+	* @param prefResults Contains a newline separated list of scary files
+	*/
+case class Brain( id: Int,                                   // primary key
+                  dateTime: java.util.Date = java.util.Date, // primary key
+                  name: String,                              // contains user's name
+                  mftCSV: String,                            // contains the mft in csv format
+                  regCSV: String,                            // contains registry in csv format.
+                  prefResults: String                        // contains any extra information.
+                ){}
+
+/**
+	* case class Findings
+	* Purpose: Contains information discovered from the assessment
+	* of the raw data.
+	* @param id primary key
+	* @param dateTime primary key Stores current date.
+	* @param mft MFTAssessment Data resulting from assessment.
+	* @param registry RegAssessment Data from registry assesment
+	* @param prefetch PrefAssessment Data from prefetch assessment
+	*/
+
+case class Findings( id: Int,                                   // primary key
+                     dateTime: java.util.Date = java.util.Date, // primary key
+                     mft: MFTAssessment,                        // contains result of MFTAssessment
+                     registry: RegAssessment,                   // contains result of Registry Assessment
+                     prefetch: PrefAssessment                   // contains result of Prefetch Assessment
+                   ){}
+
+/**
+	* case class Registration
+	* Purpose: Contains information about the client
+	* @param id primary key
+	* @param dateTime primary key Stores current date.
+	* @param user User A String name of the user/organization.
+	* @param tech Int The IP address & tech info about client
+	* @param dir String Stores location of BigBrainSecurity on client.
+	*/
+case class Registration( id: Int,                               // primary key
+                         dateTime: java.util.Date = java.util.Date, // primary key
+												 user: User,                            // name of user/organization
+                         tech: Technical,                       // client's technical details
+											   dir: String                            // location where user runs program from.
+												){}
+
+/**
+	* case class User
+	* Purpose: Contains information about the client
+	* @param id Primary key Integer ID
+	* @param address Array[String] user/organization address
+	* @param phone Int Client's
+	*/
+case class User( id: Int,                       // primary key
+                 address: Array[String],        // name of user/organization
+                 phone: Int,                    // client's technical details
+                 badStatus: Boolean = false     // location where user runs program from.
+                       ){}
+/**
+	* case class Technical
+	* Purpose: Contains technical information about the client
+	* @param id Primary key Integer ID
+	* @param ip Int The IP address of the client
+	* @param port Int The port the client receives at.
+	*/
+case class Technical( val id: Int,
+                      val ip: Int,
+                      val port: Int,
+                      val whateverelse: String){}
 
 class BigBrainSecurity extends Setup {
 

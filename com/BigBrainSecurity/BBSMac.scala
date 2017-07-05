efaacf221788e8a0c029fce4ef3ebc8959281f6a
J. Alexander <xandermoorman@gmail.com>
2017-07-05 13:37:08 -0500

    Create BBSMac.scala

---
+++ com/BigBrainSecurity/BBSMac.scala
@@ -0,0 +1,14 @@
+package com.BigBrainSecurity
+
+/**
+	* @author j. Alexander
+	* @date 7/5/17
+	* @version 1.0
+	*
+	*      Program Purpose: Implements Big Brain Security on MacOS.
+	*/
+
+class BBSMac(val configMap: Map[String, Option[String]])  {
+
+
+} // END BBSMac class

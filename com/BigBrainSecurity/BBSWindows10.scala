764380898a9b58324fd8b5880c2a001a6c223d91
J. Alexander <xandermoorman@gmail.com>
2017-07-05 13:37:34 -0500

    Create BBSWindows10.scala

---
+++ com/BigBrainSecurity/BBSWindows10.scala
@@ -0,0 +1,16 @@
+package com.BigBrainSecurity
+
+/**
+	* @author j. Alexander
+	* @date 7/5/17
+	* @version 1.0
+	*
+	*      Program Purpose: Implements Big Brain Security on Windows 8 and later.
+	*/
+class BBSWindows10(val configMap: Map[String, Option[String]]) {
+
+	/* Clean up MFT csv with CleanMFT.scala*/
+	val cleanedMFT = new CleanMFT(spark, configMap)
+	cleanedMFT.runCleanMFT
+
+} // END BBSWindows10 class

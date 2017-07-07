1822496ed39291ae391d20bf25deede5a02f11cc
J. Alexander <xandermoorman@gmail.com>
2017-07-07 16:13:16 -0500

    Update BBSMac.scala

--- com/BigBrainSecurity/BBSMac.scala
+++ com/BigBrainSecurity/BBSMac.scala
@@ -1,12 +1,12 @@
 package com.BigBrainSecurity
 
 /**
-	* @author j. Alexander
-	* @date 7/5/17
-	* @version 1.0
-	*
-	*      Program Purpose: Implements Big Brain Security on MacOS.
-	*/
+  * @author j. Alexander
+  * @date 7/5/17
+  * @version 1.0
+  *
+  *      Program Purpose: Implements Big Brain Security on MacOS.
+  */
 
 class BBSMac(val configMap: Map[String, Option[String]])  {
 

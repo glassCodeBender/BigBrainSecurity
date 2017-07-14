04cd2fa4be0c3531301e6d9bf91e586f561b52af
J. Alexander <xandermoorman@gmail.com>
2017-07-14 06:24:50 -0500

    AutomateVolatility for Windows

--- com/BigBrainSecurity/AutomateVolatility.scala
+++ com/BigBrainSecurity/AutomateVolatility.scala
@@ -1,4 +1,6 @@
-package com.BigBrainSecurity.vol
+package com.BigBrainSecurity.vol.windows
+
+import com.BigBrainSecurity.vol.windows.AutomateVolDiscoveryWindows
 
 /** This is the main class used to automate volatility */
 object AutomateVolatility extends App {
@@ -6,10 +8,9 @@ object AutomateVolatility extends App {
   val os = "Windows7x64"
   val memFile = "/Users/glassCodeBender/Documents/memdump.mem"
 
-  val discovery = AutomateVolDiscovery.run(os, memFile)
+  val discovery = AutomateVolDiscoveryWindows.run(os, memFile)
 
   /** After we have the results, we need to interrogate each process */
-
 
 
 
